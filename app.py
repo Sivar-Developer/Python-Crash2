91560dbffaf92d8daa781dfe292a1b31b41a3b60
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-11-16 21:14:04 +0300

    Working with numbers

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ print("/____|")
 
 # Variables and data types
 character_name = "John"
-character_age = "50"
+character_age = 50
 is_male = True
 print("There once was a named " + character_name + ", ")
-print("he was " + character_age + " years old. ")
+print("he was " + str(character_age) + " years old. ")
 
 character_name = "Mike"
 print("He really liked the name " + character_name + ", ")
-print("but didn't like being " + character_age + ".")
+print("but didn't like being " + str(character_age) + ".")
 
 # Working with strings
 print("Opennbox\nAcademy")
@@ -31,3 +31,22 @@ print(len(phrase))
 print(phrase[0])
 print(phrase.index("box"))
 print(phrase.replace("Academy", "Datacenter"))
+
+# Working with numbers
+print(-2.4546)
+print(3.45 + 4.54)
+print(3.45 / 4.54)
+print(3 * (4 + 5))
+print(10 % 3)
+my_num = 5
+print(str(my_num) + " my favourite number")
+print(abs(-5))
+print(pow(4, 6))
+print(max(4, 6))
+print(min(4, 6))
+print(round(45.6543))
+
+from math import *
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
