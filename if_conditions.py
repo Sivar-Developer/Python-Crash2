6e941419bcc0650be82aef0b35023bfb60939522
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-11-17 01:00:24 +0300

    If statements and comparisons

--- if_conditions.py
+++ if_conditions.py
@@ -15,3 +15,16 @@ elif not(is_male) and is_tall:
     print("You are not a male but tall")
 else:
     print("You are either not male or not tall or not both")
+
+
+# If statements and comparisons
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(3, 400, 5))
