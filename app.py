75ef18b3a30cda9811d8e189ffb02657cca867a3
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-11-16 20:43:17 +0300

    Working with strings

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ print("/____|")
 
 # Variables and data types
 character_name = "John"
-character_age = 50
+character_age = "50"
 is_male = True
 print("There once was a named " + character_name + ", ")
 print("he was " + character_age + " years old. ")
@@ -18,3 +18,16 @@ print("he was " + character_age + " years old. ")
 character_name = "Mike"
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
+
+# Working with strings
+print("Opennbox\nAcademy")
+print("Opennbox\Academy")
+phrase = "Opennbox Academy"
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("box"))
+print(phrase.replace("Academy", "Datacenter"))
