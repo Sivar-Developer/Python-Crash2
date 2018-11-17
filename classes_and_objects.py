69ab9180286086613521ac7c81acf12c56bc287e
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-11-17 18:21:18 +0300

    Classes and Objects

---
+++ classes_and_objects.py
@@ -0,0 +1,6 @@
+# Classes and Objects
+from student import student
+student1 = student("Jim", "Business", 3.1, False)
+student2 = student("Sivar", "Developer", 2.5, True)
+print(student1.name)
+print(student2.major)
