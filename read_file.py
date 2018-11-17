7f458a776723550a8b8b911f85277e749b56bd0c
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-11-17 05:38:34 +0300

    Read from external file

---
+++ read_file.py
@@ -0,0 +1,9 @@
+# Read from external file
+employee_file = open("employees.txt", "r")
+print(employee_file.readable())
+# print(employee_file.read())
+# print(employee_file.readline())
+# print(employee_file.readlines())
+for employee in employee_file.readlines():
+    print(employee)
+employee_file.close()
