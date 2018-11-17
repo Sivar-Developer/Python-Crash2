9b3dc9fafea7226b2806392a20ca2f972266902c
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-11-17 05:05:37 +0300

    Building translator

---
+++ translator.py
@@ -0,0 +1,12 @@
+# Building translator
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
