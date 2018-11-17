542674a89af808d15a30ce3a72b81ac1667ed104
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-11-17 05:13:10 +0300

    Comments

---
+++ comments.py
@@ -0,0 +1,14 @@
+# This programme is cool
+print("Comments are fun")
+
+'''
+    This is
+    multiple line
+    comments
+'''
+
+# This is
+# multiple line
+# comments
+
+# print("Comment out code")
